a612b7b201133a6751106624e953b590b1e62668
Fathan Margono <fmgonoo@gmail.com>
2018-09-09 11:50:26 +0700

    First Commit

---
+++ src/Component/App/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const App = (rops) => {
+    return (
+        <h1>Hello World!</h1>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+    // entry: "./src/index.js",
+    // output: {
+    //     path: path.resolve("dist"),
+    //     filename: "bundle.js"
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
